2e8e9026276f9bcfb42738c90e54ab3b11833a77
Mohansai Mutyala <mohansai.career@gmail.com>
2026-07-13 23:14:24 +0530

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-         <Route path="/" element={<Navigate to="/register" replace />} />
+        <Route path="/" element={<Navigate to="/register" replace />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Login = () =>{
 
     return (
-        <div><h1>Login</h1></div>
+        <div>
+            <h1>Welcome to Login Page</h1>
+        </div>
     );
 };
 
