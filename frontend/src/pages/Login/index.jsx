098ad13c1746d8a5547c1a292975cac8691564d4
Mohansai Mutyala <mohansai.career@gmail.com>
2026-07-21 22:41:51 +0530

    Login page

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -7,6 +7,7 @@ const Login = () =>{
             <h1>Welcome to Login Page of Career OS</h1>
             <p>Please Login the portal</p>
             <p>create login</p>
+            <p>How many days will you postpone the career-os<p/>
         </div>
     );
 };
